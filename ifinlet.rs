f729eaef4d701493bdd5a8bd76ebfe4eef943f09
scr165 <45762266+scr165@users.noreply.github.com>
2021-05-16 12:02:26 +0000

    Add files via upload

---
+++ ifinlet.rs
@@ -0,0 +1,5 @@
+fn main() {
+let condition = true; //assigning a boolean variable
+let number = if condition { 5 } else { 6 }; //if expression in let
+println!("The value of number is: {}", number); //printing
+}
