a1897e7ced3997108e0e22e6c1b34ba6fa38004d
scr165 <45762266+scr165@users.noreply.github.com>
2021-05-23 04:07:42 +0000

    Delete ifinlet.rs

--- ifinlet.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-let condition = true; //assigning a boolean variable
-let number = if condition { 5 } else { 6 }; //if expression in let
-println!("The value of number is: {}", number); //printing
-}
